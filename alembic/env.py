3fb2495afb3c6068f8ea28b785598ec4795f29be
VikingVik123 <vodimmegwa@gmail.com>
2026-05-11 11:18:31 +0100

    fix/minor updates to resolves ruff errors

--- alembic/env.py
+++ alembic/env.py
@@ -16,13 +16,6 @@ config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from app.modules.templates.models.badges_model import Badge
-from app.modules.templates.models.organiser_templates_model import OrganiserTemplate
-from app.modules.templates.models.platform_templates_model import PlatformTemplate
-from app.modules.templates.models.template_hashtags_model import TemplateHashtag
-from app.models.user import User
-from app.models.auth_provider import AuthProvider
-from app.models.refresh_tokens import RefreshToken
 
 target_metadata = Base.metadata
 
